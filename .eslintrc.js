c91342c8efba92f6ecca6b92063fa7de2b6f5b27
sravanrekandar <sravanrekandar@gmail.com>
2016-03-21 09:32:02 +0530

    first pages commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    
+  ],
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "semi": [2, "never"],
+    "max-len": [1, 150]
+  },
+  "globals": {
+    "jQuery": true,
+    "$": true,
+    "R": true
+  }
+};
